75f1ee1e4def770379244e1ee597f66297f81c1d
Kayland Remy <klr232@peon13.usask.ca>
2018-04-04 12:33:25 -0600

    Misc fixes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var fs = require('fs');
 var url = require('url');
 var path = require('path');
+var json = require('JSON');
 var app = express();
 var mtg = require('mtgsdk');
 var x;
@@ -11,7 +12,15 @@ app.use('/images',express.static('images'))
 app.use('/css/',express.static('css'))
 
 app.get('/get', function(req,res) {
-	mtg.card.where(req.query).then(body => {res.send(body)})
+	mtg.card.where(req.query).then(body => {
+	for (var x of body){
+		fs.appendFile("./cards/" + x.name.replace(/\W/g, ''),json.stringify(x), function (err) {
+			if (err) throw err
+			console.log('Saved!');
+		})
+	}
+
+	res.send(body)})
 })
 app.get('/', function(req,res){
 	res.sendFile(path.join(__dirname,'html/index.html'));
